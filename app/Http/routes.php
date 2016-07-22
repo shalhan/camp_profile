0e987c64b26e2cbe9f2a2e4e5825f59367ff86f3
shalhan <shalhanradifan44@gmail.com>
2016-07-22 16:32:32 +0700

    adding usercontroller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,13 +13,16 @@
 
 
 Route::get('/', function () {return view('login');});
+//Lecture
 Route::get('dashboard', function () {return view('dosen.dashboard');});
 Route::get('activity', function () {return view('dosen.activity-table');});
 Route::get('activity-details', function () {return view('dosen.activity-details');});
 Route::get('activity-add', function () {return view('dosen.activity-add');});
+//Admin
 Route::get('admin-dashboard', function () {return view('admin.dashboard');});
 Route::get('admin-activity-lecture', function () {return view('admin.activity-lecture');});
 Route::get('admin-activity-student', function () {return view('admin.activity-student');});
+//Student
 Route::get('student-dashboard', function () {return view('student.dashboard');});
 Route::get('student-activity-details', function () {return view('student.activity-details');});
 Route::get('student-activity-add', function () {return view('student.activity-add');});
